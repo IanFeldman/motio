c038e00960e5a00a38ebbef1fbca4311fc11334d
IanFeldman <ianrfeldman@gmail.com>
2025-03-27 18:09:38 -0700

    Refactor - remove traits

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ use sdl3::event::Event;
 use sdl3::image::LoadTexture;
 use sdl3::keyboard::Keycode;
 use sdl3::Error;
+use sdl3::render::FRect;
 use std::path::Path;
 use std::time::Duration;
 
 mod objects;
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), Error>
+{
     /* create sdl context */
     let sdl_context = sdl3::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -34,23 +36,28 @@ fn main() -> Result<(), Error> {
     let texture = texture_creator.load_texture(path)?;
 
     /* create vectors of objects */
-    let mut objects: Vec<&mut dyn objects::Object> = Vec::new();
+    let mut objects: Vec<&mut objects::Object> = Vec::new();
     /* create square */
-    let mut square_object = objects::Square::new(
-        objects::Transform::new(0.0, 0.0, 0.0, 0.0, 1.0),
-        objects::Sprite::new(64.0, 64.0, texture));
+    let mut obj = objects::Object::new(
+        objects::Transform::new(400.0, 300.0, 10.0, 10.0, 1.0),
+        objects::Sprite::new(64.0, 64.0, texture),
+        objects::ObjectType::Normal);
     /* push to vector */
-    objects.push(&mut square_object);
+    objects.push(&mut obj);
 
     /* run loop, check events */
     let mut event_pump = sdl_context.event_pump().unwrap();
-    'running: loop {
+    'running: loop
+    {
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
+        for event in event_pump.poll_iter()
+        {
+            match event
+            {
                 Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } =>
+                {
                     break 'running
                 },
                 _ => {}
@@ -67,15 +74,35 @@ fn main() -> Result<(), Error> {
 
 /* objects must be mut in order to use iter_mut */
 fn main_loop(canvas: &mut sdl3::render::Canvas<sdl3::video::Window>,
-        objects: &mut Vec<&mut dyn objects::Object>)
-        -> Result<(), Error> {
+    objects: &mut Vec<&mut objects::Object>) -> Result<(), Error>
+{
     /* get delta time */
     let delta = 1.0 / 60.0;
     /* iterate over objects */
-    for object in objects.iter_mut() {
-        object.physics_update(delta);
-        object.draw(canvas)?;
+    for object in objects.iter_mut()
+    {
+        objects::update(object, delta);
+        draw(canvas, object)?;
     }
+    Ok(())
+}
+
+fn draw(canvas: &mut sdl3::render::Canvas<sdl3::video::Window>,
+    object: &mut objects::Object) -> Result<(), Error>
+{
+    let pos_x = object.transform.x - object.sprite.width / 2.0;
+    let pos_y = object.transform.y - object.sprite.height / 2.0;
+
+    canvas.copy_ex(
+        &object.sprite.texture,
+        None,
+        /* TODO: consider saving rect to struct to avoid this overhead */
+        FRect::new(pos_x, pos_y, object.sprite.width, object.sprite.height),
+        object.transform.theta as f64,
+        None,
+        false,
+        false
+    )?;
     Ok(())
 }
 
--- src/objects.rs
+++ src/objects.rs
@@ -1,20 +1,23 @@
-use sdl3::Error;
-use sdl3::render::{FRect, Texture};
+use sdl3::render::Texture;
 
-pub struct Sprite {
+pub struct Sprite
+{
     pub width: f32,
     pub height: f32,
     pub texture: Texture
 }
 
-impl Sprite {
-    pub fn new(width: f32, height: f32, texture: Texture) -> Self {
+impl Sprite
+{
+    pub fn new(width: f32, height: f32, texture: Texture) -> Self
+    {
         Sprite { width, height, texture }
     }
 }
 
 /* Position, angle, and physics properties */
-pub struct Transform {
+pub struct Transform
+{
     pub x: f32,
     pub y: f32,
     pub theta: f32,
@@ -23,47 +26,46 @@ pub struct Transform {
 }
 
 /* Implement constructor and default physics update */
-impl Transform {
-    pub fn new(x: f32, y: f32, theta: f32, angular_velocity: f32, mass: f32) -> Self {
+impl Transform
+{
+    pub fn new(x: f32, y: f32, theta: f32, angular_velocity: f32, mass: f32) -> Self
+    {
         Transform { x, y, theta, angular_velocity, mass }
     }
-    pub fn update(&mut self, delta_time: f32) {
+    pub fn update(&mut self, delta_time: f32)
+    {
         /* apply angular velocity */
         self.theta += self.angular_velocity * delta_time;
     }
 }
 
-/* Object trait implements physics update and draw */
-pub trait Object {
-    fn physics_update(&mut self, delta_time: f32);
-    fn draw(&self, canvas: &mut sdl3::render::Canvas<sdl3::video::Window>)
-        -> Result<(), Error>;
+pub enum ObjectType
+{
+    Normal,
+    Spring,
+    Static
 }
 
-/* Individual object types */
-pub struct Square {
+/* Object types */
+pub struct Object
+{
     pub transform: Transform,
     pub sprite: Sprite,
+    pub object_type: ObjectType
+    /* collider */
 }
 
-impl Square {
-    pub fn new(transform: Transform, sprite: Sprite) -> Self {
-        Square { transform, sprite }
+impl Object
+{
+    pub fn new(transform: Transform, sprite: Sprite, object_type: ObjectType) -> Self
+    {
+        Object { transform, sprite, object_type }
     }
 }
 
-impl Object for Square {
-    fn physics_update(&mut self, delta_time: f32) {
-        /* square specific physics */
-        self.transform.x += 10.0 * delta_time;
-        self.transform.y += 10.0 * delta_time;
-        /* transform update */
-        self.transform.update(delta_time);
-    }
-    fn draw(&self, canvas: &mut sdl3::render::Canvas<sdl3::video::Window>)
-        -> Result<(), Error> {
-        canvas.copy(&self.sprite.texture, None, FRect::new(self.transform.x, self.transform.y, self.sprite.width, self.sprite.height))?;
-        Ok(())
-    }
+pub fn update(object: &mut Object, delta_time: f32)
+{
+    /* check which object type, perform update */
+    object.transform.update(delta_time);
 }
 
