276f2af41cb9f305f38a8c0a1d3b8cc6b22f4cef
IanFeldman <ianrfeldman@gmail.com>
2025-03-31 23:40:08 -0700

    Make create gear function

--- src/main.rs
+++ src/main.rs
@@ -63,35 +63,8 @@ fn main() -> Result<(), Error>
 
     /* create vector of objects */
     let mut objects: Vec<objects::Object> = Vec::new();
-    let mut collider: Vec<objects::SphereCollider> = Vec::new();
-    collider.push(objects::SphereCollider::new(0.0, 0.0, 32.0));
-    /* create gear */
-    let obj1 = objects::Object::new(
-        objects::Transform::new(0.0, 0.0, 32.0, 0.0, 1.0),
-        objects::Sprite::new(64.0, 64.0, 0),
-        collider,
-        objects::ObjectType::Spring(5.0));
-    /* create gear */
-    /* consider how to reuse collider */
-    let mut collider: Vec<objects::SphereCollider> = Vec::new();
-    collider.push(objects::SphereCollider::new(0.0, 0.0, 32.0));
-    let obj2 = objects::Object::new(
-        objects::Transform::new(0.0, 60.0, 180.0, 0.0, 1.0),
-        objects::Sprite::new(64.0, 64.0, 0),
-        collider,
-        objects::ObjectType::Normal);
-    /* create gear */
-    let mut collider: Vec<objects::SphereCollider> = Vec::new();
-    collider.push(objects::SphereCollider::new(0.0, 0.0, 32.0));
-    let obj3 = objects::Object::new(
-        objects::Transform::new(10.0, 120.0, 0.0, 0.0, 1.0),
-        objects::Sprite::new(64.0, 64.0, 0),
-        collider,
-        objects::ObjectType::Normal);
-    /* push to vector */
-    objects.push(obj1);
-    objects.push(obj2);
-    objects.push(obj3);
+    create_gear(0.0, 0.0, 22.5, 100.0, objects::ObjectType::Spring(50.0), &mut objects);
+    create_gear(60.0, 0.0, 0.0, 100.0, objects::ObjectType::Normal, &mut objects);
 
     /* run loop, check events */
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -262,5 +235,19 @@ fn draw(canvas: &mut sdl3::render::Canvas<sdl3::video::Window>,
     }
 
     Ok(())
+}
+
+/* create gear object and add it to list of all objects */
+fn create_gear(x: f32, y: f32, theta: f32, mass: f32, object_type: objects::ObjectType,
+    objects: &mut Vec<objects::Object>)
+{
+    let mut collider: Vec<objects::SphereCollider> = Vec::new();
+    collider.push(objects::SphereCollider::new(0.0, 0.0, 32.0));
+    let gear = objects::Object::new(
+        objects::Transform::new(x, y, theta, 0.0, mass),
+        objects::Sprite::new(64.0, 64.0, 0),
+        collider,
+        object_type);
+    objects.push(gear);
 }
 
